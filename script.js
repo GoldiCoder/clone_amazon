27d4d2203f934f60afcd001a74921385da324e19
GoldiCoder <67798786+GoldiCoder@users.noreply.github.com>
2023-09-08 12:10:47 +0530

    Adding js file

---
+++ script.js
@@ -0,0 +1,15 @@
+/*<script>
+var foot1 = document.getElementById("foot1");
+
+window.onscroll = function() {
+    scrollFunction()};
+
+foot1.onclick = function() {
+        document.documentElement.scrollTop = 0 };// For Chrome, Firefox, IE, and Opera
+</script>*/
+
+<script>
+function returnToTop() {
+    window.location.reload() // Refreshes the page
+};
+ </script>
